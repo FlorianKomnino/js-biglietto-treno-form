67e1d722127a1022bcea75a3d3bdfbd033c63f7c
FlorianKomnino <florian.komnino@outlook.it>
2022-11-22 17:58:17 +0100

    added comments and started building button.addEventListener

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 console.log('Hello World!');
 
+// Assegno input lunghezza viaggio inserita dall'utente a una variabile
 const tripLength = document.getElementById('tripLength');
+// Assegno input età inserita dall'utente a una variabile
 const userAge = document.getElementById('userAge');
+// Assegno prezzo unitario al chilometro
 const pricePerKm = 0.2762;
 
+// Assegno html button a una variabile
+const button = document.getElementById('buyButton');
+
+// add event listener sulla variabile del button
+button.addEventListener('click', function() {
+    console.log(userAge.value);
+    console.log(tripLength.value);
+
+    console.log(parseInt(tripLength.value) * pricePerKm);
+})
